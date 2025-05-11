0e3f12ace071ab5be9175bc714aeb309b77b8ee1
Daniel Kapin <danielkapin@gmail.com>
2025-05-10 19:24:03 -0500

    Fixed liniting errors

--- bin/run.js
+++ bin/run.js
@@ -4,7 +4,7 @@ import {execute} from '@oclif/core'
 
 // Force loading from the manifest for TypeScript projects
 await execute({
+  development: false,
   dir: import.meta.url,
-  useManifest: true,
-  development: false
+  useManifest: true
 })
